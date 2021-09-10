236a3dc30e6951c1f927d873d2914188303bc102
saradeveloper92 <sara.elifani92@gmail.com>
2021-09-10 14:56:57 +0200

    implementazione con arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class Main {
+   public static ArrayList listaParole = new ArrayList();
+    static boolean programContinue = false;
+
     public static void main(String[] args){
+        do{
+            String[] fraseInIngresso = splitFraseInParole();
+            riempiLista(fraseInIngresso);
+            programContinue= true;
+        }while (programContinue);
+
+    }
+
+    public static String[] splitFraseInParole() {
+        String input = JOptionPane.showInputDialog(null, "Inserisci la frase");
+        String[] arrayParole = input.split(" ");
+        return arrayParole;
+    }
+
+    public static void riempiLista(String[] arrayParoleInput){
+        for (int i=0; i < arrayParoleInput.length; i++) {
+            listaParole.add(arrayParoleInput[i]);
+        }
+        Iterator<Integer> myIterator2 = listaParole.iterator();
+        while (myIterator2.hasNext()) {
+            Object myItem = myIterator2.next();
+            System.out.println(myItem);
+        }
+        System.out.println("------------");
 
     }
 }
